889fd9a17d626aafca4a53a57a6bbc8054d8864e
Was <Was@Was-PC>
2016-10-09 15:39:31 -0300

    Commit do dia 09/10/2016

--- src/main/java/com/washington/cobranca/controller/TituloController.java
+++ src/main/java/com/washington/cobranca/controller/TituloController.java
@@ -1,15 +1,19 @@
 package com.washington.cobranca.controller;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ErrorAttributes;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.washington.cobranca.model.StatusTitulo;
 import com.washington.cobranca.model.Titulo;
@@ -21,27 +25,41 @@ public class TituloController {
 
 	@Autowired
 	Titulos titulos;
+	private static final String CADASTRO_VIEW = "CadastroTitulo";
 	
 	@RequestMapping("/novo")
 	public ModelAndView novo() {
-		ModelAndView mv = new ModelAndView("CadastroTitulo");		
+		
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
+		mv.addObject(new Titulo());
 		return mv;
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public ModelAndView salvar(Titulo titulo) {
-		//TODO: salvar no banco
-		titulos.save(titulo);
-		ModelAndView mv = new ModelAndView("CadastroTitulo");
-		mv.addObject("message","Titulo salvo com sucesso!");
+	public String salvar(@Validated Titulo titulo,Errors erros,RedirectAttributes redirectAttributes) {		
+		if(erros.hasErrors()) {
+			return CADASTRO_VIEW;
+		}
 		
-		return mv;
+		
+		titulos.save(titulo);
+		redirectAttributes.addFlashAttribute("message","Titulo salvo com sucesso!");
+		return "redirect:/titulos/novo";
 	}
 	
 	@RequestMapping("/pesquisa")
 	public ModelAndView pesquisarTitulo() {
 		ModelAndView mv = new ModelAndView("PesquisaTitulo");		
 		mv.addObject("Titulos", titulos.findAll().toArray());
+		return mv;
+	}
+	
+	@RequestMapping("{codigo}") 
+	public ModelAndView edicao(@PathVariable Long codigo) {
+		
+		Titulo titulo = titulos.findOne(codigo);
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
+		mv.addObject(titulo);		
 		return mv;
 	}
 	
--- src/main/java/com/washington/cobranca/model/Titulo.java
+++ src/main/java/com/washington/cobranca/model/Titulo.java
@@ -12,7 +12,12 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
+import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.NumberFormat;
 
@@ -28,14 +33,22 @@ public class Titulo {
 	@Column(name = "id_titulo")
 	private long idTitulo;
 	
+	@NotNull(message = "Data de vencimento é obrigatória.")
 	@Temporal(TemporalType.DATE)
 	@DateTimeFormat(pattern = "dd/MM/yyyy")
 	@Column(name = "data_vencimento")
 	private Date dataVencimento;
 	
+	@NotEmpty(message = "Descrição é obrigatória")
+	@Size(max = 60,message = "A descrição não pode conter mais de 60 caracteres")
 	private String descricao;
+	
+	@NotNull(message = "Valor é obrigatório")
+	@DecimalMin(value = "0.01",message = "Valor não pode ser menor que 0,01")
+	@DecimalMax(value = "9999999999.99", message = "Valor não pode ser maior que 9.999.999.999,99")
 	@NumberFormat(pattern = "#,##0.00")
 	private BigDecimal valor;
+	
 	@Enumerated(EnumType.STRING)
 	private StatusTitulo status;
 	
